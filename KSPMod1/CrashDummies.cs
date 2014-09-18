91c1957adfa97b243c4a77653bc438592937649a
johnqevil <ryan.pershing@gmail.com>
2014-09-18 09:54:54 -0700

    First Upload

---
+++ KSPMod1/CrashDummies.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+using KSP;
+
+namespace CrashDummies
+{
+    public class CrashDummy : PartModule
+
+    {
+        public override void OnStart(PartModule.StartState state)
+        {
+            if (HighLogic.LoadedSceneIsFlight == true)
+            {
+                RemoveDummy();
+            }
+            base.OnStart(state);
+        }
+        public void RemoveDummy()
+        {
+            foreach (PartResource res in part.Resources)
+            {
+                if (res.info.name == "CrashDummies")
+                {
+                    res.amount = res.amount - 1;
+                }
+            }
+         
+        }
+    }
+
+}
